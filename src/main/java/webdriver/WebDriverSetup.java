1a5659dc314e914cfc7a8f9c5d4849087d6283e3
Shubhojit Bhattacharjee <shubhojit.bhattacharjee@thoughtfocus.com>
2019-10-19 22:44:36 +0530

    - Add WebDriverSetup for Chrome and implementation of Geckodriver and IEDriver - Add parallel execution - Remove asynchronous wait

---
+++ src/main/java/webdriver/WebDriverSetup.java
@@ -0,0 +1,78 @@
+package webdriver;
+
+import com.sun.javafx.PlatformUtil;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WebDriverSetup {
+	
+	private static final String LIB_DIR = System.getProperty("user.dir") + "\\src\\lib\\webdrivers\\";
+
+
+	public static WebDriver getWebDriver(String browserName) {
+		WebDriver driver;
+		if ("CH".equals(browserName)) {
+			driver = getChromeDriver();
+		} else if ("FF".equals( browserName )) {
+			driver = getFirefoxDriver();
+		} else if ("IE".equals( browserName )) {
+			driver = getInternetExplorerDriver();
+		} else
+			return null;
+		return driver;
+	}
+
+	private static WebDriver getChromeDriver() {
+
+		if (PlatformUtil.isMac()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver");
+		}
+		if (PlatformUtil.isWindows()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		}
+		if (PlatformUtil.isLinux()) {
+			System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+		}
+
+		ChromeOptions co = new ChromeOptions();
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put( "profile.default_content_setting_values.notifications", 2 );
+		co.setExperimentalOption( "prefs", prefs );
+		co.addArguments( "--start-maximized" );
+		return new ChromeDriver( co );
+	}
+	
+	private static WebDriver getFirefoxDriver() {
+		
+		System.setProperty("webdriver.gecko.driver", LIB_DIR + "geckodriver.exe");
+
+		return new FirefoxDriver();
+	}
+
+	private static WebDriver getInternetExplorerDriver() {
+		try {
+			System.setProperty("webdriver.ie.driver", LIB_DIR + "IEDriverServer.exe");
+			DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+			capabilities.setCapability("nativeEvents", false);
+			capabilities.setCapability( InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+			//capabilities.setCapability("Ignore",InternetExplorerUnexpectedAlertBehavior.Ignore);
+			capabilities.setCapability( CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+			return new InternetExplorerDriver();
+                    //InternetExplorerDriver(capabilities);
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+		return null;
+	}
+	
+}
--- src/test/tests/BaseTest.java
+++ src/test/tests/BaseTest.java
@@ -1,67 +1,25 @@
 package tests;
 
-import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import webdriver.WebDriverSetup;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
-    @BeforeTest(alwaysRun = true)
-    protected void setDriverPath() {
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("browser")
+    protected void setUp(String browser) {
 
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-        ChromeOptions co = new ChromeOptions();
-        Map<String, Object> prefs = new HashMap<String, Object>();
-        prefs.put("profile.default_content_setting_values.notifications", 2);
-        co.setExperimentalOption("prefs", prefs);
-        co.addArguments("--start-maximized");
-        driver = new ChromeDriver(co);
+        driver = WebDriverSetup.getWebDriver(browser);
     }
 
-    public BaseTest() {
-
-        PageFactory.initElements( driver, this );
-    }
-
-    protected void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-
-    protected boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    @AfterTest(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     protected void cleanUp() {
         driver.quit();
+        driver = null;
     }
 }
